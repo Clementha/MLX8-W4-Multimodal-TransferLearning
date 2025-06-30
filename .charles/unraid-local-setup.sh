798d23db4267bee18bbe8b1bec0cf46b3319e213
Charles Cai <charles.cai@socialogix.net>
2025-06-30 17:56:20 +0100

    updated history length in shell setup.sh

--- .charles/unraid-local-setup.sh
+++ .charles/unraid-local-setup.sh
@@ -22,3 +22,10 @@ scp -r /Users/charles/Library/CloudStorage/OneDrive-Personal/_digital/__media__/
 
 # Run from MacOS to copy the flaticon data to the unraid RAPIDS docker
 # scp -r /Users/charles/Library/CloudStorage/OneDrive-Personal/_digital/__media__/flaticon.com/ socialogix-unraid1:/workspace/_github/MLX8-W4-Multimodal-TransferLearning/.charles/.data/
+
+# Example: rsync local MLX8-W4-Multimodal-TransferLearning to unraid server
+rsync -avz -e "ssh -p 2230" /workspace/_github/MLX8-W4-Multimodal-TransferLearning/ root@192.168.50.243:/mnt/cache/__tmp/workspace/_github/charles-cai/MLX8-W4-Multimodal-TransferLearning/
+
+# Increase bash history size for Ubuntu
+echo 'HISTSIZE=10000' >> ~/.bashrc
+echo 'HISTFILESIZE=20000' >> ~/.bashrc
